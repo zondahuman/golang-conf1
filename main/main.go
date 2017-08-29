b99d31da27c43be4bc9e0234a2644b623690781e
zondahuman <zondahuman@gmail.com>
2017-08-30 01:06:21 +0800

    add

--- main/main.go
+++ main/main.go
@@ -9,13 +9,13 @@ import (
 
 //引入数据模型
 func init() {
-	confName := flag.String("confName", "golang-dev.ini", "this is conf name")
+	confName := flag.String("confName", "local", "this is conf name")
 
 
 	flag.Parse()
 	fmt.Println("confName:", *confName)
 
-	constant.GlobalConfName = *confName
+	constant.GlobalConfName = "golang-"+*confName+".ini"
 	fmt.Println("GlobalConfName....:", constant.GlobalConfName)
 	conf := goini.SetConfig("./conf/"+constant.GlobalConfName) //goini.SetConfig(filepath) 其中filepath是你ini 配置文件的所在位置
 	constant.GlobalConf = *conf
